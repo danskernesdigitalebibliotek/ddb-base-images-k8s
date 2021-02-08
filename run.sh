c8b2f54f6b8c8f042250790ebd35b6edd42a047a
Jesper Kristensen <cableman@linuxdev.dk>
2021-02-08 09:24:46 +0100

    DDBTEAM-718: Fixed run script to build images

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#docker build --no-cache --tag="danskernesdigitalebibliotek/php7.3-fpm-base:latest" --file="php7.3-fpm-base/Dockerfile" php7.3-fpm-base
+docker build --no-cache --tag="danskernesdigitalebibliotek/php7.3-fpm-base:latest" --file="php7.3-fpm-base/Dockerfile" php7.3-fpm-base
 docker build --no-cache --tag="danskernesdigitalebibliotek/php7.4-fpm-base:latest" --file="php7.4-fpm-base/Dockerfile" php7.4-fpm-base
-#docker build --no-cache --tag="danskernesdigitalebibliotek/elasticsearch_exporter:latest" --file="elasticsearch_exporter/Dockerfile" elasticsearch_exporter
+docker build --no-cache --tag="danskernesdigitalebibliotek/elasticsearch_exporter:latest" --file="elasticsearch_exporter/Dockerfile" elasticsearch_exporter
 
-#docker push danskernesdigitalebibliotek/php7.3-fpm-base:latest
-#docker push danskernesdigitalebibliotek/php7.4-fpm-base:latest
-#docker push danskernesdigitalebibliotek/elasticsearch_exporter:latest
+docker push danskernesdigitalebibliotek/php7.3-fpm-base:latest
+docker push danskernesdigitalebibliotek/php7.4-fpm-base:latest
+docker push danskernesdigitalebibliotek/elasticsearch_exporter:latest
