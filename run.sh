5fb34778be3525f45124a1da6d4d4dedbb9bb9b7
Jesper Kristensen <cableman@linuxdev.dk>
2021-07-06 11:13:54 +0200

    DDBTEAM-798: Update build script with version

--- run.sh
+++ run.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+VERSION=1.0.0
+
 docker build --no-cache --tag="danskernesdigitalebibliotek/php7.3-fpm-base:latest" --file="php7.3-fpm-base/Dockerfile" php7.3-fpm-base
 docker build --no-cache --tag="danskernesdigitalebibliotek/php7.4-fpm-base:latest" --file="php7.4-fpm-base/Dockerfile" php7.4-fpm-base
 docker build --no-cache --tag="danskernesdigitalebibliotek/elasticsearch_exporter:latest" --file="elasticsearch_exporter/Dockerfile" elasticsearch_exporter
 
+docker image tag danskernesdigitalebibliotek/php7.3-fpm-base danskernesdigitalebibliotek/php7.3-fpm-base:${VERSION}
+docker push danskernesdigitalebibliotek/php7.3-fpm-base:${VERSION}
 docker push danskernesdigitalebibliotek/php7.3-fpm-base:latest
+
+docker image tag danskernesdigitalebibliotek/php7.4-fpm-base danskernesdigitalebibliotek/php7.4-fpm-base:${VERSION}
+docker push danskernesdigitalebibliotek/php7.4-fpm-base:${VERSION}
 docker push danskernesdigitalebibliotek/php7.4-fpm-base:latest
+
+docker image tag danskernesdigitalebibliotek/elasticsearch_exporter danskernesdigitalebibliotek/elasticsearch_exporter:${VERSION}
+docker push danskernesdigitalebibliotek/elasticsearch_exporter:${VERSION}
 docker push danskernesdigitalebibliotek/elasticsearch_exporter:latest
