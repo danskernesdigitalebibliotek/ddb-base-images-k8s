489ec7d64fe4e19ec829a9193acedd238df9ed67
Jesper Kristensen <cableman@linuxdev.dk>
2022-03-08 11:09:21 +0100

    DDBTEAM-945: Added timezone and php 8.1 image

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-VERSION=1.0.5
+VERSION=1.0.6
 
 docker build --no-cache --tag="danskernesdigitalebibliotek/php7.3-fpm-base:latest" --file="php7.3-fpm-base/Dockerfile" php7.3-fpm-base
 docker build --no-cache --tag="danskernesdigitalebibliotek/php7.4-fpm-base:latest" --file="php7.4-fpm-base/Dockerfile" php7.4-fpm-base
 docker build --no-cache --tag="danskernesdigitalebibliotek/php8.0-fpm-base:latest" --file="php8.0-fpm-base/Dockerfile" php8.0-fpm-base
+docker build --no-cache --tag="danskernesdigitalebibliotek/php8.0-fpm-base:latest" --file="php8.1-fpm-base/Dockerfile" php8.1-fpm-base
+
 
 docker build --no-cache --tag="danskernesdigitalebibliotek/elasticsearch_exporter:latest" --file="elasticsearch_exporter/Dockerfile" elasticsearch_exporter
 
@@ -19,6 +21,10 @@ docker push danskernesdigitalebibliotek/php7.4-fpm-base:latest
 docker image tag danskernesdigitalebibliotek/php8.0-fpm-base danskernesdigitalebibliotek/php8.0-fpm-base:${VERSION}
 docker push danskernesdigitalebibliotek/php8.0-fpm-base:${VERSION}
 docker push danskernesdigitalebibliotek/php8.0-fpm-base:latest
+
+docker image tag danskernesdigitalebibliotek/php8.1-fpm-base danskernesdigitalebibliotek/php8.1-fpm-base:${VERSION}
+docker push danskernesdigitalebibliotek/php8.1-fpm-base:${VERSION}
+docker push danskernesdigitalebibliotek/php8.1-fpm-base:latest
 
 docker image tag danskernesdigitalebibliotek/elasticsearch_exporter danskernesdigitalebibliotek/elasticsearch_exporter:${VERSION}
 docker push danskernesdigitalebibliotek/elasticsearch_exporter:${VERSION}
