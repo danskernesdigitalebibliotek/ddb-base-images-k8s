b67dc8eb04ffbb42ba3fdb9f225985ffee40e6d7
Jesper Kristensen <cableman@linuxdev.dk>
2023-02-01 16:01:03 +0100

    Bumped image version

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 set -e
 set -x
 
-VERSION=1.0.18
+VERSION=1.0.19
 
-docker build --no-cache --tag="danskernesdigitalebibliotek/php7.3-fpm-base:latest" --file="php7.3-fpm-base/Dockerfile" php7.3-fpm-base
-docker build --no-cache --tag="danskernesdigitalebibliotek/php7.4-fpm-base:latest" --file="php7.4-fpm-base/Dockerfile" php7.4-fpm-base
-docker build --no-cache --tag="danskernesdigitalebibliotek/php8.0-fpm-base:latest" --file="php8.0-fpm-base/Dockerfile" php8.0-fpm-base
-docker build --no-cache --tag="danskernesdigitalebibliotek/php8.1-fpm-base:latest" --file="php8.1-fpm-base/Dockerfile" php8.1-fpm-base
+docker build --pull --no-cache --tag="danskernesdigitalebibliotek/php7.3-fpm-base:latest" --file="php7.3-fpm-base/Dockerfile" php7.3-fpm-base
+docker build --pull --no-cache --tag="danskernesdigitalebibliotek/php7.4-fpm-base:latest" --file="php7.4-fpm-base/Dockerfile" php7.4-fpm-base
+docker build --pull --no-cache --tag="danskernesdigitalebibliotek/php8.0-fpm-base:latest" --file="php8.0-fpm-base/Dockerfile" php8.0-fpm-base
+docker build --pull --no-cache --tag="danskernesdigitalebibliotek/php8.1-fpm-base:latest" --file="php8.1-fpm-base/Dockerfile" php8.1-fpm-base
 
-docker build --no-cache --tag="danskernesdigitalebibliotek/elasticsearch_exporter:latest" --file="elasticsearch_exporter/Dockerfile" elasticsearch_exporter
+docker build --pull --no-cache --tag="danskernesdigitalebibliotek/elasticsearch_exporter:latest" --file="elasticsearch_exporter/Dockerfile" elasticsearch_exporter
 
 docker image tag danskernesdigitalebibliotek/php7.3-fpm-base danskernesdigitalebibliotek/php7.3-fpm-base:${VERSION}
 docker push danskernesdigitalebibliotek/php7.3-fpm-base:${VERSION}
